b276b511b1ba66eb1a67062fb4ed6ba887d392cd
DevEd2 <deved8@gmail.com>
2018-04-27 16:16:27 -0400

    re-added cleanup section

--- build.bat
+++ build.bat
@@ -14,6 +14,8 @@ rgblink -p 255 -o %PROJECTNAME%_GBS.gbc %PROJECTNAME%_GBS.obj
 if errorlevel 1 goto :BuildError
 echo Fixing...
 rgbfix -v -p 255 %PROJECTNAME%.gbc
+echo Cleaning up...
+del %PROJECTNAME%.obj
 echo Build complete.
 rem Build GBS file
 echo Building GBS file...
@@ -24,10 +26,12 @@ del /f %PROJECTNAME%_GBS.obj %PROJECTNAME%_GBS.gbc
 goto :end
 
 :BuildError
+set PROJECTNAME=
 echo Build failed, aborting...
 goto:eof
 
 :GBSMakeError
+set PROJECTNAME=
 echo GBS build failed, aborting...
 goto:eof
 
